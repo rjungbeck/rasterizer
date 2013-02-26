4102014e2468d87075e72a51a163415fdabce68a
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2013-02-26 23:41:51 +0100

    Convert to current MuPdf version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 import py2exe
 
 
-setup(name="PDF Rasterizer",
+setup(name="PDF-Rasterizer",
 	version="1.00.0000",
 	description="Convert PDF into PNG and PRN",
 	author="Ruediger Jungbeck, RSJ Software GmbH",
 	author_email="ruediger.jungbeck@rsj.de",
-	url="http://ww.rsj.de",
+	url="http://www.rsj.de",
 	console=["rasterizer.py"],
-	data_files=[("",["libmupdf.dll", r"dll\mfc90.dll", r"dll\mfc90u.dll", r"dll\mfcm90.dll", r"dll\mfcm90u.dll", r"dll\Microsoft.VC90.MFC.manifest"])],
+	data_files=[("",["COPYING", "libmupdf.dll", r"dll\mfc90.dll", r"dll\mfc90u.dll", r"dll\mfcm90.dll", r"dll\mfcm90u.dll", r"dll\Microsoft.VC90.MFC.manifest"])],
 	zipfile=None,
 	options = {"py2exe": { "includes": [], 
 		"dll_excludes": [""],
