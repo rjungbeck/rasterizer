da840f5fd1f08cf2272c6772377284791268b71d
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2013-07-26 13:04:21 +0200

    Support for Windows XP

---
+++ mupdf.py
@@ -0,0 +1,15 @@
+import sys
+
+from mupdfold import MuPdf as MuPdfOld
+from mupdfnew import MuPdf as MuPdfNew
+
+class MuPdf():
+	def __init__(self):
+		if sys.getwindowsversion().major>5:
+			self.__class__=MuPdfNew
+			self.__init__(self)
+		else:
+			self.__class__=MuPdfOld
+			self.__init__()
+		
+		
