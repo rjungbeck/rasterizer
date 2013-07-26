a806a41122ad8667846329192dce9f47b3ce0899
Stefan Beutler <stefan.beutler@rsj.de>
2013-07-26 14:24:59 +0200

    added source for (old) libmupdf.dll 2.2.1 (needed for XP), including libmupdf22.dll built by RSJ

--- mupdf.py
+++ mupdf.py
@@ -7,7 +7,7 @@ class MuPdf():
 	def __init__(self):
 		if sys.getwindowsversion().major>5:
 			self.__class__=MuPdfNew
-			self.__init__(self)
+			self.__init__()
 		else:
 			self.__class__=MuPdfOld
 			self.__init__()
