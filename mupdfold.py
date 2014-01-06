59e0d2c887a7a2187363d5824a64c9f220b62bad
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2014-01-06 21:29:28 +0100

    Thumbnail capability

--- mupdfold.py
+++ mupdfold.py
@@ -112,18 +112,35 @@ class MuPdf():
 	def loadPage(self, num):
 		self.page=self.dll.fz_load_page(self.doc, num-1)
 		
-	def render(self,name,angle=0,resolution=300.0, xDelta=0.0, yDelta=0.0, aaLevel=-1):
+	def render(self,name,angle=0,resolution=300.0, xDelta=0.0, yDelta=0.0, aaLevel=-1,maxWidth=0,maxHeight=0,colorSpace="DeviceGray"):
 		if aaLevel!=-1:
 			self.dll.fz_set_aa_level(self.context, aaLevel)
+			
+		rect=self.dll.fz_bound_page(self.doc, self.page)
+		
+		w=abs(rect.x0-rect.x1)*resolution/72.0
+		h=abs(rect.y0-rect.y1)*resolution/72.0
+		if angle==90 or angle==270:
+			w,h=h,w
+			
+		f=1.0
+		if maxWidth:
+			if w>maxWidth:
+				f=maxWidth*1.0/w
+				
+		if maxHeight:
+			if h>maxHeight:
+				f=min(f, maxHeight*1.0/h)
+		
+		resolution*=f
 			
 		transform=self.dll.fz_scale(resolution/72.0, resolution/72.0)
 		transform=self.dll.fz_concat(transform, self.dll.fz_rotate(angle)) 
 		transform=self.dll.fz_concat(transform, self.dll.fz_translate(xDelta, yDelta))
 		
-		rect=self.dll.fz_bound_page(self.doc, self.page)
 		rect=self.dll.fz_transform_rect(transform, rect)
 		bbox=self.dll.fz_round_rect(rect)
-		fz_device_rgb=self.dll.fz_find_device_colorspace(self.context, "DeviceGray")
+		fz_device_rgb=self.dll.fz_find_device_colorspace(self.context, colorSpace)
 		pix=self.dll.fz_new_pixmap_with_bbox(self.context, fz_device_rgb, bbox)
 		self.dll.fz_clear_pixmap_with_value(self.context, pix, 255)
 		dev=self.dll.fz_new_draw_device(self.context, pix)
