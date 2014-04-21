73db416d72bb6a923eec0321d3b6eb29b8b6a215
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2014-04-21 15:59:08 +0200

    New --binaryUpload and --config option in build.py (works only for RSJ)

--- build.py
+++ build.py
@@ -15,7 +15,13 @@ def pythonRelCall(relCommand, command, cwd=None):
 
 def main():
 	parser=argparse.ArgumentParser(description="Rasterizer Builder", epilog="(C) Copyright 2013 by RSJ Software GmbH Germering. All rights reserved.")
+	parser.add_argument("--config", type=str, default="build.json", help="Build configuration")
+	parser.add_argument("--uploadBinary", action="store_true", help="Upload installer to S3")
+	
 	parms=parser.parse_args()
+	
+	with open(parms.config, "r") as f:
+		config=json.load(f)
 		
 	with open("version.json", "r") as f:
 		version=json.load(f)
@@ -39,6 +45,15 @@ def main():
 	
 	# Inno Setup
 	subprocess.call(r'"c:\Program Files (x86)\Inno Setup 5\iscc.exe" -dversion=' + version + ' rasterizer.iss')
+	
+	if parms.uploadBinary:
+		import boto
+		from boto.s3.key import Key
+		conn=boto.connect_s3(config["awsKeyId"], config["awsSecretKey"])
+		bucket=conn.get_bucket(config["s3Bucket"])
+		target=Key(bucket=bucket, name=config["s3KeyPrefix"]+"rasterizerInst-"+version+".exe")
+		target.set_contents_from_filename("output/rasterizerInst.exe")
+		target.make_public()
 	
 if __name__=="__main__":
 	main()
