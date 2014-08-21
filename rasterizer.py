7048a2392c8c7a19f89147ec16d4fb35f79e689b
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2014-08-21 10:12:19 +0200

    Batch handler included (initial commit) SSL support (initial commit) Config file for rastersvc (Initial commit)

--- rasterizer.py
+++ rasterizer.py
@@ -160,6 +160,7 @@ def main():
 	parserConvert.set_defaults(func=convert)
 	
 	parserServe=subparsers.add_parser("serve", help="Serve")
+	parserServe.add_argument("--config", type=str, default="conf/rastersvc.json", help="Config file")
 	parserServe.add_argument("--port", type=int, default=8080, help="Server port")
 	parserServe.set_defaults(func=serve)
 	
--- rastersvc.py
+++ rastersvc.py
@@ -9,10 +9,12 @@ class RasterizerService(servicebase.ServiceBase):
 	_svc_name_ = "RasterizerService"
 	_svc_display_name_ = "Rasterizer"
 	_svc_deps_=["tcpip"]
-	options={"-p": {"default":"8000", "name":"serverPort", "help":"Server Port"}}
+	options={"-p": {"default":"8000", "name":"serverPort", "help":"Server Port"},
+			"-c": {"default": "rastersvc.json", "name":"configFile", "help":"Config File"}}
 	
 	def  ServiceRun(self):
 		self.port=int(self.getOption("port", default="8000"))
+		self.configPath=self.getOption("configPath", default="rastersvc.json")
 		serve(self)
 
 	def ServiceStop(self):
